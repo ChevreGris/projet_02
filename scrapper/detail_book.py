5ed6e511d7238dbc39a5130c6e2e71caf89fcd7f
mark.d <dumet.mark@gmail.com>
2021-09-08 23:29:37 +0200

    projet_02 v0.02.0 (second commit)

--- scrapper/detail_book.py
+++ scrapper/detail_book.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+#def get_book_info(url):
+
 url = 'http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html'
 
 response = requests.get (url)
@@ -8,14 +10,11 @@ response = requests.get (url)
 
 if response.ok:
     soup = BeautifulSoup(response.text, 'html.parser')
-
-
     tds = soup.findAll('td')
     universal_product_code = tds[0]
     price_including_tax = tds[2]
     price_excluding_tax = tds[3]
     number_available = tds[5]
-
 
     titres = soup.findAll('h1')
     titre = titres[0]
@@ -27,18 +26,41 @@ if response.ok:
     categorie = categories[3]
     
     image = soup.find(class_='item active')
+    image_str = str(image)
+    image_incomplete_url1 = image_str.replace('<div class="item active">','')
+    image_incomplete_url2 = image_incomplete_url1.replace('<img alt="Tipping the Velvet" src="../..', 'http://books.toscrape.com')
+    image_incomplete_url3 = image_incomplete_url2.replace('"/>','')
+    image_url = image_incomplete_url3.replace('</div>', '')
 
+    def product_rating(rating):
+        if ('One') in rating:
+            rating = "1/5"
+        elif ('Two') in rating:
+            rating = '2/5'
+        elif ('Three') in rating:
+            rating = '3/5'
+        elif ('Four') in rating:
+            rating = '4/5'
+        elif ('Five') in rating:
+            rating = '5/5'
+        else:
+            rating = None
+        return rating
 
+    rating = str(soup.find('p', class_="star-rating"))
+    
+
+    
+"""
+    page_url = url
 
     print('UPC : ' + universal_product_code.text)
     print('PRIX HT : ' + price_including_tax.text)
     print('PRIX TTC : ' + price_excluding_tax.text)
     print('STOCK : ' + number_available.text)
-
     print('TITRE : ' + titre.text)
-    
     print('CATEGORIE : ' + categorie.text)
-
     print('DESCRIPTION : ' + descript.text)
+    print('url image ' + image_url)
 
-    #print('URL IMAGE : ' + str(image))
+"""
