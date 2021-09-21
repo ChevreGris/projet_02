d162cb346777f8624e2701d13592e3a3d910d015
mark.d <dumet.mark@gmail.com>
2021-09-21 17:38:38 +0200

    PROJET_02 v0.10.0 (opti)

--- scrapper/__main__.py
+++ scrapper/__main__.py
@@ -1,51 +1,44 @@
+from os import write
 import requests
 from bs4 import BeautifulSoup
 
 from detail_book_info import get_book_info
 from detail_book_url import get_all_books_links
-from detail_book_url import get_book_url
+#from detail_book_url import get_book_url
 from detail_category_url import get_category_urls
-
+from detail_category_url import get_category_names
 if __name__ == '__main__':
 
     main_url = 'http://books.toscrape.com'
-    #url_categories = 'http://books.toscrape.com/catalogue/category/books/childrens_11/index.html'
-    #url_book = 'http://books.toscrape.com/catalogue/the-grownup_546/index.html'
+    url_categorie = 'http://books.toscrape.com/catalogue/category/books/childrens_11/index.html'
+
+
+
+
+    #'''
+    def categories_data(url_de_la_categorie):
+        links = get_all_books_links(url_de_la_categorie)
+        data = []
+
+        for url in links:
+            data.append(get_book_info(url))
+        return data
+    #'''
+
+    #i = categories_data(url_categorie)
+    #print(i)
     
-    def test(main_url):
+    #'''
+    categories_url = get_category_urls(main_url)
+    categories_names = get_category_names(main_url)
 
-        ioi = []
+    
+    for url,names in categories_url,categories_names:
+        file = open(names + '.txt', 'w+')
+        file.write(str(categories_data(url)))
+        file.close()
 
-        categories_links = get_category_urls(main_url)
-        for links in categories_links:
-            ioi.extend(get_all_books_links(links))
-
-        return ioi
-
-
-    uio = test(main_url)
-    print(uio)
+    #'''
 
 
-
-
-
-
-
-
-    #test_url = get_book_url(url_categories)
-    #print(test_url)
-
-    #urls = get_category_urls(main_url)
-    #print(urls)
-
-
-    #url_book = get_all_books_links(url_categories)
-    #print(url_book)
-
-    #pages_nbrs = get_all_categories_pages(url_categories)
-    #print(pages_nbrs)
-
-    #info = get_book_info(url_book)
-    #print(info)
 
--- scrapper/detail_category_url.py
+++ scrapper/detail_category_url.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+"""
+#fonction qui trouve les urls de chaque categories.
 def get_category_urls(main_url):
     response = requests.get (main_url)
     if response.ok:
@@ -10,9 +12,36 @@ def get_category_urls(main_url):
 
         for category in soup.select(".nav.nav-list ul a"):
             categories.append('http://books.toscrape.com/' + category["href"])
-
+            categories.append(category.text.strip())
         return categories
+        
+"""
 
+#"""
+#fonction qui trouve les urls de chaque categories.
+def get_category_urls(main_url):
+    response = requests.get (main_url)
+    if response.ok:
+        soup = BeautifulSoup(response.text, 'html.parser')
 
+        categories = []
 
-   
+        for category in soup.select(".nav.nav-list ul a"):
+            categories.append('http://books.toscrape.com/' + category["href"])
+        return categories
+        
+#"""
+
+#"""
+#fonction qui trouve les noms de chaque categories.
+def get_category_names(main_url):
+    response = requests.get (main_url)
+    if response.ok:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        categories_names = []
+
+        for category in soup.select(".nav.nav-list ul a"):
+            categories_names.append(category.text.strip())
+        return categories_names
+#"""
